137c515804ab5df1292bdda342ed23a4faa27c48
Brian Rushd and Hunter Parks <brian+hunter@>
2017-06-27 15:59:06 -0700

    clean up code and add explanatory comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 //business logic stuff for a particular module
+
+//CURRENT TIME OBJECT/MODULE
 var CurrentTimeModule = function() {
   // this.currentTime = moment().format("LTS");
 };
 
+//GET AND FORMAT CURRENT TIME
 CurrentTimeModule.prototype.currentTime = function() {
   return moment().format("LT");
 };
 
+//GET AND FORMAT CURRENT TIME WITH SECONDS (FOR CONSOLE)
 CurrentTimeModule.prototype.currentSeconds = function() {
   return moment().format("LTS");
 };
 
 
-
+//EXPORT THE MODULE
 exports.currentTimeModule = CurrentTimeModule;
